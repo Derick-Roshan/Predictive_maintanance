2d419b4f17d72b43c54d3a827fba0a4ee1665d80
Roshan R <86513599+Derick-Roshan@users.noreply.github.com>
2025-10-04 23:43:17 +0530

    i have done some critical changes
    make sure it is correct or not. if it not correct i modify the code again and ill push u

--- demo.py
+++ demo.py
@@ -5,3 +5,6 @@ print(c "both the thongs are added")
 print (a)
 print("his is the one the python file created fot the learning purpose")
 print("three print line is added in the dev branch")
+
+
+print("Just checking from the git that is accepting the push option")
