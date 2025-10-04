b3d791c0053f88e38bcc55ef97fc2666a9ccea34
Roshan <roshansmesh@gmail.com>
2025-10-04 23:51:37 +0530

    change are done finally

--- demo.py
+++ demo.py
@@ -6,9 +6,4 @@ print (a)
 print("his is the one the python file created fot the learning purpose")
 print("three print line is added in the dev branch")
 
-
-<<<<<<< HEAD
 print("this cahnges i did from the git bash")
-=======
-print("Just checking from the git that is accepting the push option")
->>>>>>> 2d419b4f17d72b43c54d3a827fba0a4ee1665d80
