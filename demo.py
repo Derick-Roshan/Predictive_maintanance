9afb1b99e9c33dd0e560b29f21686852c80f1d23
Roshan <roshansmesh@gmail.com>
2025-10-04 23:38:32 +0530

    Here a and b is added and valued c

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 a=10
 b=20
+c=a+b
+print(c "both the thongs are added")
 print (a)
 print("his is the one the python file created fot the learning purpose")
 print("three print line is added in the dev branch")
