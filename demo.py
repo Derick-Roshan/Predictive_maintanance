0dacfccb176c9dc2bacf98f247cc32350f189bcb
Roshan <roshansmesh@gmail.com>
2025-10-11 23:33:34 +0530

    I just deleted every code inside this demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,1 @@
-a=10
-b=20
-c=a+b
-print(c "both the thongs are added")
-print (a)
-print("his is the one the python file created fot the learning purpose")
-print("three print line is added in the dev branch")
-
-print("this cahnges i did from the git bash")
+print("This file is temprory i kept htis for some study purposE)
